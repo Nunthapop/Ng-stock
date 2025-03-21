13b9b1a03a551c2a80a9e94f116ac8e58223bca5
Arthur <nuntapopsangehot@gmail.com>
2025-03-21 16:59:28 +0700

    Update wallet component

--- src/app/component/wallet/wallet.component.ts
+++ src/app/component/wallet/wallet.component.ts
@@ -1,12 +1,30 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
+import { CurrencyService } from '../../services/currency.service'; // Import the service
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms'; // Import ReactiveFormsModule and FormBuilder
+
 @Component({
   selector: 'app-wallet',
-  imports: [SharedModule],
+  imports: [SharedModule, CommonModule, ReactiveFormsModule], // Add ReactiveFormsModule to imports
   templateUrl: './wallet.component.html',
   styleUrl: './wallet.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WalletComponent {
+  currencyData$!: Observable<any>;
+  currencyForm: FormGroup;
 
+  constructor(private currencyService: CurrencyService, private fb: FormBuilder) { // Inject FormBuilder
+    this.currencyForm = this.fb.group({
+      base: [''],
+      target: ['']
+    });
+  }
+
+  getCurrency() {
+    const { base, target } = this.currencyForm.value;
+    this.currencyData$ = this.currencyService.getCurrency(base, target); // Use the service with form values
+  }
 }
